0f064444af11b70fa16e411f313ad73939cdb297
Kallen Tu <kallentu@gmail.com>
2020-01-20 23:21:34 -0500

    Added error handling.

--- dartox.dart
+++ dartox.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'package:args/args.dart';
 
+bool hadError = false;
+
 main(List<String> args) {
   final ArgParser argParser = new ArgParser()
     ..addOption('source', abbr: 's', help: "The file you want to compile.");
@@ -12,9 +14,13 @@ main(List<String> args) {
     exit(64);
   } else if (argResults.arguments.length == 1) {
     _run(argResults['file']);
+    if (hadError) exit(65);
   } else {
     // Run interactive prompt
     _runPrompt();
+
+    // If user makes error, we shouldn't close the entire session.
+    hadError = false;
   }
 }
 
@@ -34,4 +40,13 @@ void _run(String source) {
 //    for (Token token : tokens) {
 //      System.out.println(token);
 //    }
+}
+
+void error(int line, String message) {
+  _report(line, "", message);
+}
+
+void _report(int line, String where, String message) {
+  print("[line $line] Error $where : $message");
+  hadError = true;
 }
