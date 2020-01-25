bd344de8a753c60f887240cb3d26e1c79e72c0b3
Kallen Tu <kallentu@gmail.com>
2020-01-25 16:36:22 -0500

    Advance token and add to list of tokens.

--- lib/src/scanner.dart
+++ lib/src/scanner.dart
@@ -1,3 +1,5 @@
+import "package:charcode/ascii.dart";
+
 import 'package:dartox/src/token.dart';
 import 'package:dartox/src/token_type.dart';
 
@@ -15,12 +17,55 @@ class Scanner {
     while(!_isAtEnd()) {
       // We are at the beginning of the next lexeme.
       _start = _current;
-      // TODO: scanToken();
+      _scanToken();
     }
 
     _tokens.add(new Token(TokenType.EOF, "", null, _line));
     return _tokens;
   }
 
+  void _scanToken() {
+    int c = _advance();
+    switch(c) {
+      case $lparen:
+        _addToken(TokenType.LEFT_PAREN);
+        break;
+      case $rparen:
+        _addToken(TokenType.RIGHT_PAREN);
+        break;
+      case $lbrace:
+        _addToken(TokenType.LEFT_BRACE);
+        break;
+      case $rbrace:
+        _addToken(TokenType.RIGHT_BRACE);
+        break;
+      case $comma:
+        _addToken(TokenType.COMMA);
+        break;
+      case $dot:
+        _addToken(TokenType.DOT);
+        break;
+      case $minus:
+        _addToken(TokenType.MINUS);
+        break;
+      case $plus:
+        _addToken(TokenType.PLUS);
+        break;
+      case $semicolon:
+        _addToken(TokenType.SEMICOLON);
+        break;
+      case $asterisk:
+        _addToken(TokenType.STAR);
+        break;
+    }
+  }
+
   bool _isAtEnd() => _current >= _source.length;
+
+  int _advance() => _source.codeUnitAt(++_current - 1);
+
+  void _addToken(TokenType type, {Object literal}) {
+    String text = _source.substring(_start, _current);
+    _tokens.add(new Token(type, text, literal, _line));
+  }
 }
