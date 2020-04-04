c7695ba25be564cd05a364d5bf7fa69343f6860c
Kallen Tu <kallentu@gmail.com>
2020-04-04 02:15:53 -0400

    Reserved words and multi-line comments.

--- lib/src/scanner.dart
+++ lib/src/scanner.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import "package:charcode/ascii.dart";
 import 'package:dartox/src/error.dart';
 import 'package:dartox/src/token.dart';
@@ -13,7 +15,28 @@ class Scanner {
   int _current = 0;
   int _line = 0;
 
-  Scanner(this._source);
+  final Map<String, TokenType> _keywords = new HashMap<String, TokenType>();
+
+
+  Scanner(this._source) {
+    // Initialize the static keywords.
+    _keywords.putIfAbsent("and", () => TokenType.AND);
+    _keywords.putIfAbsent("class", () => TokenType.CLASS);
+    _keywords.putIfAbsent("else", () => TokenType.ELSE);
+    _keywords.putIfAbsent("false", () => TokenType.FALSE);
+    _keywords.putIfAbsent("for", () => TokenType.FOR);
+    _keywords.putIfAbsent("fun", () => TokenType.FUN);
+    _keywords.putIfAbsent("if", () => TokenType.IF);
+    _keywords.putIfAbsent("nil", () => TokenType.NIL);
+    _keywords.putIfAbsent("or", () => TokenType.OR);
+    _keywords.putIfAbsent("print", () => TokenType.PRINT);
+    _keywords.putIfAbsent("return", () => TokenType.RETURN);
+    _keywords.putIfAbsent("super", () => TokenType.SUPER);
+    _keywords.putIfAbsent("this", () => TokenType.THIS);
+    _keywords.putIfAbsent("true", () => TokenType.TRUE);
+    _keywords.putIfAbsent("var", () => TokenType.VAR);
+    _keywords.putIfAbsent("while", () => TokenType.WHILE);
+  }
 
   List<Token> scanTokens() {
     while (!_isAtEnd()) {
@@ -73,8 +96,24 @@ class Scanner {
         break;
       case $slash:
         if (_match($slash)) {
-          // Comments go until end of the line
+          // // comments go until end of the line
           while (_peek() != $lf && !_isAtEnd()) _advance();
+        } else if (_match($asterisk)) {
+          // /* ... */ comments can continue multiple lines.
+          // Move until comment ends.
+          while (_peek() != $asterisk && _peekNext() != $slash && !_isAtEnd()) {
+            if (_peek() == $lf) _line++;
+            _advance();
+          }
+
+          // Unterminated comment
+          if (_isAtEnd()) {
+            return;
+          }
+
+          // Move past * and /
+          _advance();
+          _advance();
         } else {
           _addToken(TokenType.SLASH);
         }
@@ -94,11 +133,26 @@ class Scanner {
       default:
         if (_isDigit(c)) {
           _number();
+        } else if (_isAlpha(c)) {
+          _identifier();
         } else {
           // Keeps scanning after reporting error, but will not execute code.
           _errorReporter.error(_line, "Unexpected character.");
         }
         break;
+    }
+  }
+
+  void _identifier() {
+    while (_isAlphaNumeric(_peek())) _advance();
+
+    // Check if identifier is a reserved word in [keywords]
+    String text = _source.substring(_start, _current);
+    if (_keywords.containsKey(text)) {
+      TokenType type = _keywords[text];
+      _addToken(type);
+    } else {
+      _addToken(TokenType.IDENTIFIER);
     }
   }
 
@@ -137,6 +191,11 @@ class Scanner {
     if (_current + 1 >= _source.length) return $nul;
     return _source.codeUnitAt(_current + 1);
   }
+
+  bool _isAlpha(int c) =>
+      (c >= $a && c <= $z) || (c >= $A && c <= $Z) || c == $_;
+
+  bool _isAlphaNumeric(int c) => _isAlpha(c) || _isDigit(c);
 
   bool _isDigit(int c) => c >= $0 && c <= $9;
 
